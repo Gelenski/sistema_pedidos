2e9306946474f0effd8c5d326abab7b8e81908bf
Luan da Silva Saviski <saviski.luan@pucpr.edu.br>
2026-09-24 20:50:20 -0300

    Adicionado as funcionalidades das classes Pedido e ItemPedido

---
+++ src/main/java/pedido/ItemPedido.java
@@ -0,0 +1,30 @@
+package pedido;
+
+public class ItemPedido {
+
+    private String nomeProduto;
+    private int quantidade;
+    private double precoUnitario;
+
+    public ItemPedido(String nomeProduto, int quantidade, double precoUnitario) {
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double getSubtotal() {
+        return quantidade * precoUnitario;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+}
